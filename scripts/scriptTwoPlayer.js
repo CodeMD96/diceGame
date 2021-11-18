90ea9bd4d574dabcf972bf2d6180d1659126c53f
CodeMD96 <codemd96@gmail.com>
2021-11-18 11:35:22 +0000

    improved 2 player

--- scripts/scriptTwoPlayer.js
+++ scripts/scriptTwoPlayer.js
@@ -1,13 +1,29 @@
 const gameCounter = document.getElementsByClassName("win-tracker");
-const startAgain = document.getElementById("start-again");
+const start = document.getElementById("start");
+const targetLabel = document.getElementById("target-label");
+const target = document.getElementById("target-number");
 const heading = document.getElementById("heading");
 const scoreCards = document.getElementsByClassName("scorecard");
 const diceImg = document.getElementById("dice-img")
 const roll = document.getElementById("roll");
+const call = document.getElementById("call");
 
 let currentPlayer = 0;
 let scores = [0, 0];
 let playerWins = [0, 0];
+let targetNumber = 20;
+
+const playerChange = () =>{
+    if (currentPlayer === 0) {
+        currentPlayer =1;
+        heading.innerHTML = "Player 2's turn";
+        document.body.style.backgroundColor = "purple";
+    } else {
+        currentPlayer =0;
+        heading.innerHTML = "Player 1's turn";
+        document.body.style.backgroundColor = "indigo";
+    };
+};
 
 roll.addEventListener("click", () =>{
     let diceRoll = Math.floor(Math.random()*6+1);
@@ -17,40 +33,44 @@ roll.addEventListener("click", () =>{
     diceImg.src = `images/dice${diceRoll}.png`
 
     if (diceRoll === 1) {
-        playerWins[`${currentPlayer}`] -= 1;
+        scores[`${currentPlayer}`] = 0;
+        scoreCards[`${currentPlayer}`].innerHTML = scores[`${currentPlayer}`];
 
-        startAgain.innerHTML = "Too bad, try again!";
-        heading.innerHTML = `Player ${currentPlayer+1} lost!`;
-        gameCounter[`${currentPlayer}`].innerHTML = `Player ${currentPlayer+1}: ${playerWins[currentPlayer]}`;
-        startAgain.style.display = "block";
-        roll.style.display = "none";
-    } else if (scores[`${currentPlayer}`] > 20) {
-        playerWins[`${currentPlayer}`] += 1;
+        playerChange();
+    } else if (scores[`${currentPlayer}`] >= targetNumber) {
+        playerWins[`${currentPlayer}`] ++;
 
-        startAgain.innerHTML = "Start again!";
+        start.innerHTML = "Start again!";
+        targetLabel.innerHTML = "Choose target number:"
         heading.innerHTML = `Player ${currentPlayer+1} wins!`;
         gameCounter[`${currentPlayer}`].innerHTML = `Player ${currentPlayer+1}: ${playerWins[currentPlayer]}`;
-        startAgain.style.display = "block";
+        start.style.display = "block";
+        target.style.display = "block";
         roll.style.display = "none";
-    } else if (currentPlayer === 0) {
-        currentPlayer = 1;
-        heading.innerHTML = "Player 2";
-        document.body.style.backgroundColor = "purple";
-    } else {
-        currentPlayer = 0;
-        heading.innerHTML = "Player 1";
-        document.body.style.backgroundColor = "indigo";
+        call.style.display = "none";
     };
 });
 
-startAgain.addEventListener("click", () =>{
-    currentPlayer = 0;
-    scores = [0, 0];
+call.addEventListener("click", () =>{
+    playerChange();
+})
 
-    heading.innerHTML = "Player 1";
+start.addEventListener("click", () =>{
+    console.log(Number(target.value));
+    if (typeof Number(target.value) === "number" && Number(target.value)) {
+        currentPlayer = 0;
+    scores = [0, 0];
+    targetNumber = target.value;
+
     document.body.style.backgroundColor = "indigo";
+    heading.innerHTML = "Player 1's turn";
+    targetLabel.innerHTML = `Target number: ${targetNumber}`;
+
     scoreCards[0].innerHTML = 0;
     scoreCards[1].innerHTML = 0;
-    startAgain.style.display = "none";
+    start.style.display = "none";
+    target.style.display = "none";
     roll.style.display = "block";  
-})
+    call.style.display = "block";
+    };
+});
